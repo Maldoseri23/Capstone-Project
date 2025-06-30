92d5cd2171638bbe1dc5291b364f391b5e22669b
nmq2111 <nooraq123@gmail.com>
2025-06-30 15:52:46 +0300

    Calendar doneeee git add .!

--- project4/main_app/urls/event_url.py
+++ project4/main_app/urls/event_url.py
@@ -4,10 +4,9 @@ from main_app import views
 urlpatterns = [
         #event
         path('events/event_list' , views.event_list , name='event_list'),
-        path('events/<int:Event_id>/' , views.event_detail , name='event_detail'),
+        path('api/events/' , views.event_list_calender , name='event_list_calender'),
+        path('events/<int:pk>' , views.event_detail , name='event_detail'),
         path('events/create' , views.EventCreate.as_view() , name='EventCreate'),
         path('events/<int:pk>/update' , views.EventEdit.as_view() , name='EventEdit'),
         path('events/<int:pk>/delete' , views.EventDelete.as_view() , name='EventDelete'),
-        path('events/json/', views.event_json, name='event_json'),
-
 ]
