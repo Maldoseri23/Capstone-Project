3c36b8029247c68dbd37dcb5abb388620892c88d
Maldoseri23 <maldoseria@gmail.com>
2025-07-01 09:28:45 +0300

    completed garden and profile views

--- project4/main_app/views/__init__.py
+++ project4/main_app/views/__init__.py
@@ -4,3 +4,11 @@ from .pages_views import home , calendar
 from .lessons_views import lessons_by_language, lesson_detail, complete_lesson
 from .guessgame_views import game_home, get_random_word, check_guess, show_answer, reset_score, leaderboard
 from .garden_views import my_garden, award_garden_item
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from ..models import Profile
+
+@login_required
+def profile_page(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'profile/profile_page.html', {'profile': profile})
