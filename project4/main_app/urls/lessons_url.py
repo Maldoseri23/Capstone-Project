14bebc7aa091caf1d9a988fc64a06790addc2ec2
nmq2111 <nooraq123@gmail.com>
2025-07-02 14:13:16 +0300

    this is comments and rating

--- project4/main_app/urls/lessons_url.py
+++ project4/main_app/urls/lessons_url.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('lessons/<str:language_code>/', views.lessons_by_language, name='lessons_by_language'),
     path('lessons/<str:language_code>/<int:pk>/', views.lesson_detail, name='lesson_detail'),
     path('lessons/<str:language_code>/<int:pk>/complete/', views.complete_lesson, name='complete_lesson'),
+    path('lessons/<str:language_code>/<int:pk>/comment/<int:comment_id>/edit/', views.edit_comment , name='edit_comment'),
+    path('lessons/<str:language_code>/<int:pk>/comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
 ]
