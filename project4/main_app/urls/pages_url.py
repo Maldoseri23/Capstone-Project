7ecceeba261372c2ad75c7ea84d8ee520d677866
maryam027 <mariam.ali027@outlook.com>
2025-07-02 14:52:06 +0300

    add styling of pages

--- project4/main_app/urls/pages_url.py
+++ project4/main_app/urls/pages_url.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from main_app.views import pages_views as views
 from main_app.views.NameGame_views import name_game_view
-from main_app.views.quiz_views import quiz_view, quiz_submit
+
 
 
 
@@ -11,8 +11,7 @@ urlpatterns = [
         path('calendar/', views.calendar, name='calendar'),
         path('sign-to-text/', views.sign_to_text_view, name='sign_to_text'),
         path('name-game/', name_game_view, name='name-game'),
-        path('quiz/', quiz_view, name='quiz'),
-        path('quiz/submit/', quiz_submit, name='quiz_submit'),
+
        
         
 ]
