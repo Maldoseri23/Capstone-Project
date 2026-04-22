d6c9b39e0cc51090a952dbde0e0c62450bf45d04
nmq2111 <nooraq123@gmail.com>
2026-04-22 21:17:38 +0300

    Clean requirements

--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)%69azzew76kl4y$ge7#e1$rvmg9&&t9--&$13bp7q0kgd6dub'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -133,8 +133,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
