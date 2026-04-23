69ad31a20281ba0e4163ed5c5b06face2b087289
nmq2111 <nooraq123@gmail.com>
2026-04-24 02:53:54 +0300

    fixing models

--- project4/main_app/models.py
+++ project4/main_app/models.py
@@ -18,31 +18,6 @@ class Profile(models.Model):
         return self.user.username
 
 
-class Video(models.Model):
-    title = models.CharField(max_length=100)
-    video_url = models.URLField(max_length=200)
-    is_tutorial = models.BooleanField() 
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-    def __str__(self):
-        return self.title  
-
-
-class ForumPost(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField() 
-    created_at = models.DateTimeField(auto_now_add=True)
- 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-    def __str__(self):
-        return self.title
-
 
 class Event(models.Model):
     title = models.CharField(max_length=100)
